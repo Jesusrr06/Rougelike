7c22cf4f474eaeb4f26719ab77466aeb5f8022a6
Jesusrr06 <jrodrem422@g.educaand.es>
2026-01-21 13:37:37 +0100

    step 7.5
    Stuck with movements

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -2,23 +2,31 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 public class BoardManager : MonoBehaviour
 {
+    private Grid m_Grid;
     private Tilemap m_Tilemap;
     public int Width;
     public int Height;
+    private CellData[,] m_BoardData;
     public Tile[] GroundTiles;
 
+
     public Tile[] WallTiles;
-    private CellData[,] m_BoardData;
     public class CellData
     {
         public bool Passable;
     }
 
+    public Vector3 CellToWorld(Vector2Int cellIndex)
+    {
+        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
+    }
 
-
+    public PlayerController Player;
     void Start()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
+        m_Grid = GetComponentInChildren<Grid>();
+
         m_BoardData = new CellData[Width, Height];
 
         for (int y = 0; y < Height; ++y)
@@ -26,6 +34,7 @@ public class BoardManager : MonoBehaviour
             for (int x = 0; x < Width; ++x)
             {
                 Tile tile;
+                m_BoardData[x, y] = new CellData();
 
                 if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
                 {
@@ -37,15 +46,23 @@ public class BoardManager : MonoBehaviour
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
                     m_BoardData[x, y].Passable = true;
-
                 }
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
+        Player.Spawn(this, new Vector2Int(1, 1));
+
     }
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        if (cellIndex.x < 0 || cellIndex.x >= Width
+            || cellIndex.y < 0 || cellIndex.y >= Height)
+        {
+            return null;
+        }
 
-
-
+        return m_BoardData[cellIndex.x, cellIndex.y];
+    }
 
     // Update is called once per frame
     void Update()
