139a6acc98ad2955f8221d6512d01c513d3434d1
Jesusrr06 <jrodrem422@g.educaand.es>
2026-01-28 20:57:25 +0100

    Animation step 1

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using System.Collections.Generic ;
+using System.Collections.Generic;
 
 public class BoardManager : MonoBehaviour
 {
     public class CellData
-    {   
+    {
         public bool Passable;
         public CellObject ContainedObject;
     }
@@ -19,51 +19,84 @@ public class BoardManager : MonoBehaviour
 
     public List<Vector2Int> m_EmptyCellsList;
     public Tile[] WallTiles;
-   
+
     public PlayerController Player;
-    public FoodObject[] FoodPrefab= new FoodObject[2]; 
+    public ExitCellObject ExitCellPrefab;
+
+    public FoodObject[] FoodPrefab = new FoodObject[2];
     public WallObject[] WallPrefab = new WallObject[3];
+
     public Vector3 CellToWorld(Vector2Int cellIndex)
     {
         return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
     }
 
-        public void Init()
+    public void Init()
+    {
+        m_Tilemap = GetComponentInChildren<Tilemap>();
+        m_Grid = GetComponentInChildren<Grid>();
+
+        m_EmptyCellsList = new List<Vector2Int>();
+
+        m_BoardData = new CellData[Width, Height];
+
+        for (int y = 0; y < Height; ++y)
         {
-            m_Tilemap = GetComponentInChildren<Tilemap>();
-            m_Grid = GetComponentInChildren<Grid>();
-
-            m_EmptyCellsList = new List<Vector2Int>();
-
-            m_BoardData = new CellData[Width, Height];
-
-            for (int y = 0; y < Height; ++y)
+            for (int x = 0; x < Width; ++x)
             {
-                for (int x = 0; x < Width; ++x)
-                {
-                    Tile tile;
-                    m_BoardData[x, y] = new CellData();
+                Tile tile;
+                m_BoardData[x, y] = new CellData();
 
-                    if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
-                    {
-                        tile = WallTiles[Random.Range(0, WallTiles.Length)];
-                        m_BoardData[x, y].Passable = false;
-                    }
-                     else
-                        {
-                            tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
-                            m_BoardData[x, y].Passable = true;
-                            m_EmptyCellsList.Add(new Vector2Int(x, y));
-                    }    
-          
-                    m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
+                {
+                    tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    m_BoardData[x, y].Passable = false;
                 }
+                else
+                {
+                    tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                    m_BoardData[x, y].Passable = true;
+                    m_EmptyCellsList.Add(new Vector2Int(x, y));
+                }
+
+                m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
-         m_EmptyCellsList.Remove(new Vector2Int(1, 1));
-         GenerateWall();
-         GenerateFood();
-  
+        }
+        m_EmptyCellsList.Remove(new Vector2Int(1, 1));
+        Vector2Int endCoord = new Vector2Int(Width - 2, Height - 2);
+        AddObject(Instantiate(ExitCellPrefab), endCoord);
+        m_EmptyCellsList.Remove(endCoord);
+
+        GenerateWall();
+        GenerateFood();
+
     }
+    public void Clean()
+    {
+        //no board data, so exit early, nothing to clean
+        if (m_BoardData == null)
+            return;
+
+
+        for (int y = 0; y < Height; ++y)
+        {
+            for (int x = 0; x < Width; ++x)
+            {
+                var cellData = m_BoardData[x, y];
+
+                if (cellData.ContainedObject != null)
+                {
+                    //CAREFUL! Destroy the GameObject NOT just cellData.ContainedObject
+                    //Otherwise what you are destroying is the JUST CellObject COMPONENT
+                    //and not the whole gameobject with sprite
+                    Destroy(cellData.ContainedObject.gameObject);
+                }
+
+                SetCellTile(new Vector2Int(x, y), null);
+            }
+        }
+    }
+
 
     void AddObject(CellObject obj, Vector2Int coord)
     {
@@ -72,7 +105,7 @@ public class BoardManager : MonoBehaviour
         data.ContainedObject = obj;
         obj.Init(coord);
     }
-   
+
     public CellData GetCellData(Vector2Int cellIndex)
     {
         if (cellIndex.x < 0 || cellIndex.x >= Width
@@ -81,7 +114,7 @@ public class BoardManager : MonoBehaviour
             return null;
         }
 
-         return m_BoardData[cellIndex.x, cellIndex.y];
+        return m_BoardData[cellIndex.x, cellIndex.y];
     }
     void GenerateWall()
     {
@@ -92,33 +125,31 @@ public class BoardManager : MonoBehaviour
             Vector2Int coord = m_EmptyCellsList[randomIndex];
 
             m_EmptyCellsList.RemoveAt(randomIndex);
-            WallObject newWall = Instantiate(WallPrefab[Random.Range(0, WallPrefab.Length)]);   
+            WallObject newWall = Instantiate(WallPrefab[Random.Range(0, WallPrefab.Length)]);
             AddObject(newWall, coord);
         }
     }
-    
     void GenerateFood()
     {
-   
+
         int foodCount = 10;
         for (int i = 0; i < foodCount; ++i)
         {
             int randomIndex = Random.Range(0, m_EmptyCellsList.Count);
             Vector2Int coord = m_EmptyCellsList[randomIndex];
-      
+
             m_EmptyCellsList.RemoveAt(randomIndex);
             CellData data = m_BoardData[coord.x, coord.y];
             AddObject(Instantiate(FoodPrefab[Random.Range(0, FoodPrefab.Length)]), coord);
         }
 
     }
-    
     public void SetCellTile(Vector2Int cellIndex, Tile tile)
     {
-            m_Tilemap.SetTile(new Vector3Int(cellIndex.x, cellIndex.y, 0), tile);
+        m_Tilemap.SetTile(new Vector3Int(cellIndex.x, cellIndex.y, 0), tile);
     }
     public Tile GetCellTile(Vector2Int cellIndex)
     {
-        return m_Tilemap.GetTile<Tile>(new Vector3Int(cellIndex.x,     cellIndex.y, 0));
+        return m_Tilemap.GetTile<Tile>(new Vector3Int(cellIndex.x, cellIndex.y, 0));
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,9 @@ public class GameManager : MonoBehaviour
 
     public TurnManager TurnManager { get; private set; }
     private int m_FoodAmount = 100;
+    private int m_CurrentLevel = 1;
+    private VisualElement m_GameOverPanel;
+    private Label m_GameOverMessage;
 
     private void Awake()
     {
@@ -23,26 +26,58 @@ public class GameManager : MonoBehaviour
         Instance = this;
     }
 
-    void Start()
-    {
-        m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel");
-        m_FoodLabel.text = "Food : " + m_FoodAmount;
-        TurnManager = new TurnManager();
-        TurnManager.OnTick += OnTurnHappen;
+  void Start()
+{
+   TurnManager = new TurnManager();
+   TurnManager.OnTick += OnTurnHappen;
+  
+   m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel");
+  
+   m_GameOverPanel = UIDoc.rootVisualElement.Q<VisualElement>("GameOverPanel");
+   m_GameOverMessage = m_GameOverPanel.Q<Label>("GameOverMessage");
 
+   StartNewGame();
+}
+ public void StartNewGame()
+{
+   m_GameOverPanel.style.visibility = Visibility.Hidden;
+  
+   m_CurrentLevel = 1;
+   m_FoodAmount = 20;
+   m_FoodLabel.text = "Food : " + m_FoodAmount;
+  
+  BoardManager.Clean();
+   BoardManager.Init();
+  
+   PlayerController.Init();
+   PlayerController.Spawn(BoardManager, new Vector2Int(1,1));
+}
+
+
+    public void NewLevel()
+    {
+        BoardManager.Clean();
         BoardManager.Init();
         PlayerController.Spawn(BoardManager, new Vector2Int(1, 1));
-    }
- 
-  void OnTurnHappen()
-{
-   ChangeFood(-1);
-}
 
-public void ChangeFood(int amount)
-{
-   m_FoodAmount += amount;
-   m_FoodLabel.text = "Food : " + m_FoodAmount;
-}
+        m_CurrentLevel++;
+    }
+    void OnTurnHappen()
+    {
+        ChangeFood(-1);
+    }
+
+    public void ChangeFood(int amount)
+    {
+        m_FoodAmount += amount;
+        m_FoodLabel.text = "Food : " + m_FoodAmount;
+         if (m_FoodAmount <= 0)
+   { 
+    PlayerController.GameOver();
+       m_GameOverPanel.style.visibility = Visibility.Visible;
+         m_GameOverMessage.text = "Game Over!\n\nSurvived " + m_CurrentLevel + " days";
+
+   }
+    }
 
 }
