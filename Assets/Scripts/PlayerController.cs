caaf17d0d980269b87208048f87a7c0fa035c815
Jesusrr06 <jrodrem422@g.educaand.es>
2026-01-29 21:55:43 +0100

    Before finishing challenge of animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,62 +24,106 @@ public void Init()
 {
    m_IsGameOver = false;
 }
-    private void Update()
-    {
-   if (m_IsGameOver)
+  private Animator m_Animator;
+
+   private void Awake()
    {
-       if (Keyboard.current.enterKey.wasPressedThisFrame)
-       {
-           GameManager.Instance.StartNewGame();
-       }
-
-       return;
+       m_Animator = GetComponent<Animator>();
    }
-        Vector2Int newCellTarget = m_CellPosition;
-        bool hasMoved = false;
+    
+      public void MoveTo(Vector2Int cell, bool immediate)
+   {
+       m_CellPosition = cell;
 
-        if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+       if (immediate)
+       {
+           m_IsMoving = false;
+           transform.position = m_Board.CellToWorld(m_CellPosition);
+       }
+       else
+       {
+           m_IsMoving = true;
+           m_MoveTarget = m_Board.CellToWorld(m_CellPosition);
+       }
+      
+       m_Animator.SetBool("Moving", m_IsMoving);
+   }
+   private Vector3 m_MoveTarget;
+   private bool m_IsMoving;
+   public float MoveSpeed = 5f;
+   private void Update()
+{
+    if (m_IsGameOver)
+    {
+        if (Keyboard.current.enterKey.wasPressedThisFrame)
         {
-            newCellTarget.y += 1;
-            hasMoved = true;
-        }
-        else if (Keyboard.current.downArrowKey.wasPressedThisFrame)
-        {
-            newCellTarget.y -= 1;
-            hasMoved = true;
-        }
-        else if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
-        {
-            newCellTarget.x += 1;
-            hasMoved = true;
-        }
-        else if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
-        {
-            newCellTarget.x -= 1;
-            hasMoved = true;
+            GameManager.Instance.StartNewGame();
         }
 
-       if(hasMoved)
-        {
-   //check if the new position is passable, then move there if it is.
-        BoardManager.CellData cellData = m_Board.GetCellData(newCellTarget);
-            
-       if(cellData != null && cellData.Passable)
-        {
-            GameManager.Instance.TurnManager.Tick();
+        return;
+    }
 
-            if (cellData.ContainedObject == null)
-                {
-                     MoveTo(newCellTarget);
-                }
-                else if(cellData.ContainedObject.PlayerWantsToEnter())
-                {
-                MoveTo(newCellTarget);
+    if (m_IsMoving)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, m_MoveTarget, MoveSpeed * Time.deltaTime);
+
+        if (transform.position == m_MoveTarget)
+        {
+            m_IsMoving = false;
+            m_Animator.SetBool("Moving", false);
+            var cellData = m_Board.GetCellData(m_CellPosition);
+            if(cellData.ContainedObject != null)
                 cellData.ContainedObject.PlayerEntered();
-                }
-            }
+        }
 
+        return;
+    }
+
+    Vector2Int newCellTarget = m_CellPosition;
+    bool hasMoved = false;
+
+    if (Keyboard.current.upArrowKey.wasPressedThisFrame)
+    {
+        newCellTarget.y += 1;
+        hasMoved = true;
+    }
+    else if (Keyboard.current.downArrowKey.wasPressedThisFrame)
+    {
+        newCellTarget.y -= 1;
+        hasMoved = true;
+    }
+    else if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+    {
+        newCellTarget.x += 1;
+        hasMoved = true;
+    }
+    else if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
+    {
+        newCellTarget.x -= 1;
+        hasMoved = true;
+    }
+
+   if(hasMoved)
+    {
+ //check if the new position is passable, then move there if it is.
+        BoardManager.CellData cellData = m_Board.GetCellData(newCellTarget);
+        
+   if(cellData != null && cellData.Passable)
+    {
+        GameManager.Instance.TurnManager.Tick();
+
+        if (cellData.ContainedObject == null)
+            {
+                 MoveTo(newCellTarget);
+            }
+            else if(cellData.ContainedObject.PlayerWantsToEnter())
+            {
+            MoveTo(newCellTarget);
+            cellData.ContainedObject.PlayerEntered();
+            }
         }
 
     }
+
+}
 }
